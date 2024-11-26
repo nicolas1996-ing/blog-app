3bde1147adf9f8c4b6e86de4a078dc2595475bb9
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2024-11-26 09:26:29 -0500

    add styles, scopes and new property to blogpost model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# rails db:seed will run this file
 
 user = User.where(email: "test@gmail.com").first_or_initialize
 user.update!(
