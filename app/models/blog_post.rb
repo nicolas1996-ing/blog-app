86330c5be56d5eefd6db65b422bd702f607b1015
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2024-11-27 09:33:25 -0500

    add test for blog-post methods

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) } # if two blog posts have the same published_at date, the one that was updated last will be shown first
   scope :draft , -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= (?)", Time.current) }
   scope :scheduled, -> { where("published_at > (?)", Time.current) }
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
+
+# conditions for the methods:
+# blog_post.published? when published_at is not nil and published_at is less than or equal to Time.zone.now
+# blog_post.scheduled? when published_at is not nil and published_at is greater than Time.zone.now
+# blog_post.draft? when published_at is nil
+
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "draft? returns true for draft blog post" do 
+  # binding.irb # next and exit to continue
+  assert blog_posts(:draft).draft? # using the blog_post.yml fixture (:draft)
+ end
+
+ test "draft? returns false for published blog post" do 
+  assert_not BlogPost.new(title:"test", body: "title", published_at: Time.current).draft?
+ end
+
+ test "published? returns true for published blog post" do
+  assert published_blog_post.published?
+ end
+
+ test "published? returns false for draft blog post" do
+  assert_not BlogPost.new(title:"test", body: "title", published_at: nil).published?
+ end
+
+ test "scheduled? returns true for scheduled blog post" do
+  assert blog_posts(:scheduled).scheduled? # using the blog_post.yml fixture (:scheduled)
+ end
+
+ test "scheduled? returns false for published blog post" do
+  assert_not BlogPost.new(title:"test", body: "title", published_at: Time.current).scheduled?
+ end
+
+ # custom methods
+ 
+ def draft_blog_post 
+  BlogPost.new(title:"test", body: "title", published_at: nil)
+ end
+
+ def published_blog_post
+  BlogPost.new(title:"test", body: "title", published_at: Time.current)
+ end
 end
